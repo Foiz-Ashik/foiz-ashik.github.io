2fa3ba13fbada031f3c0553bfdbe4167e2edfebb
Foiz-Ashik <77127829+Foiz-Ashik@users.noreply.github.com>
2021-07-27 21:26:09 -0400

    Create jSEventListener.js

---
+++ jSEventListener.js
@@ -0,0 +1,15 @@
+      var num1 = document.getElementById('num1');
+      var num2 = document.getElementById('num2');
+      var answerField = document.getElementById('answerField');
+      var form =  document.getElementById('percentageCalc');
+
+      var myCalc = function() {                  
+			var a = Number(document.getElementById("num1").value);
+			var b = Number(document.getElementById("num2").value)/100;
+			alert(a*b);
+      }
+
+		form.addEventListener('submit', myCalc)
+		{
+          
+		}
